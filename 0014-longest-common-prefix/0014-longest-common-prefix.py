4e82ef5d48e64f015d7a4dcf69b3d059d47f9d16
Namya Baranwal <deepnevy@gmail.com>
2026-06-29 11:27:10 +0530

    Time: 5 ms (6.38%), Space: 12.4 MB (33.53%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -2,16 +2,16 @@ class Solution(object):
     def longestCommonPrefix(self, strs):
         answer=''
         j=0
-        flag='True'
+        flag=True
         i=0
         #NamyaB
         try:
-            while flag=='True':
+            while flag==True:
                 for i in range(len(strs)-1):
                     if strs[i][j]!= strs[i+1][j]:
-                        flag='False'
+                        flag=False
                         #answer+=strs[i][j-1]
-                if flag=='True':
+                if flag==True:
                     answer+=strs[i][j]
                 j+=1      
         except IndexError:
