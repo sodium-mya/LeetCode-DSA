f5a60c37fdec7ab577e4d96e65797c86f8263fa6
Namya Baranwal <deepnevy@gmail.com>
2026-06-29 10:12:07 +0530

    Time: 0 ms (100%), Space: 12.4 MB (33.53%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        answer=''
+        j=0
+        flag='True'
+        i=0
+        #NamyaB
+        try:
+            while flag=='True':
+                for i in range(len(strs)-1):
+                    if strs[i][j]!= strs[i+1][j]:
+                        flag='False'
+                        #answer+=strs[i][j-1]
+                if flag=='True':
+                    answer+=strs[i][j]
+                j+=1      
+        except IndexError:
+            return answer
+        return answer              
+                    
+    
+        
+                
+        
