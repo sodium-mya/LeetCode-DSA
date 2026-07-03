28a31af5def7f55a7be478bda0b2639378e7a363
Namya Baranwal <deepnevy@gmail.com>
2026-07-03 23:04:15 +0530

    Time: 3539 ms (5.12%), Space: 14.2 MB (13.46%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -14,20 +14,5 @@ class Solution(object):
             
         for l in repeated_index:
             removed_item=nums.pop(l-repeated_index.index(l))
-            nums.append(removed_item)
         uniqueint=len(unique)
-        '''k=0        
-        while True:
-            if len(nums)!=lenofnums:
-                if k not in nums:
-                    for j in range(lenofnums-uniqueint):
-                        nums.append(k)
-                else:
-                    k+=1
-            else:
-                break'''
-        nums[:]=nums[:uniqueint]
         return uniqueint
-
-        
-        
