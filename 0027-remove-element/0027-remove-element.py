065866b2cdc7b74fb0a451dab9bd22154283121d
Namya Baranwal <deepnevy@gmail.com>
2026-07-04 09:24:55 +0530

    Time: 0 ms (100%), Space: 12.4 MB (52.62%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def removeElement(self, nums, val):
         k=0
+        #sodium_mya
+        
         for i in range(len(nums)):
             if nums[i]!=val:
                 nums[k]=nums[i]
                 k+=1
         return k
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
+        
         
