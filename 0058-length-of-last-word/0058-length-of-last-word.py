82b4d29a3c7ace82942e854cb0d062be7f6a85ba
Namya Baranwal <deepnevy@gmail.com>
2026-07-04 10:21:14 +0530

    Time: 0 ms (100%), Space: 12.5 MB (16.81%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,13 +1,5 @@
 class Solution(object):
     def lengthOfLastWord(self, s):        
-        removed_spaces=s.strip()
-        lenofs=len(removed_spaces)
         #sodium-mya
-        if ' 'in removed_spaces:
-            for i in range(lenofs-1,-1,-1):
-                if removed_spaces[i]==' ':
-                    return lenofs-i-1
-        else:
-            return len(removed_spaces)
-
+        return len(s.split()[-1])
         
