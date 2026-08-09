b0489f153f05a34e8d6ad6d875f893bcd45661f0
Namya Baranwal <deepnevy@gmail.com>
2026-08-09 12:03:44 +0530

    Time: 22 ms (14.72%), Space: 12.4 MB (55.96%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -6,27 +6,19 @@ class Solution(object):
         :rtype: str
         """
         lenofs=len(s)
-        result=''
+        result_list = []
         diff=base=2*numRows-2
-        if numRows==1:
+        if numRows==1 or numRows>=lenofs:
             return s
         for i in range(numRows):
             j=i
             k=1
             while j<lenofs:
-                result+=s[j]
+                result_list.append(s[j])
                 if (k%2==0 and (base-diff)!=0) or diff==0:
                     j+=(base-diff)
                 else:
                     j+=diff
                 k+=1
             diff-=2
-        return result
-
-
-
-
-
-
-
-        
+        return "".join(result_list)
