c709d767647555bdff428f18f60a0e7972d895c1
Namya Baranwal <deepnevy@gmail.com>
2026-08-09 11:46:58 +0530

    Time: 15 ms (41.13%), Space: 12.5 MB (19.27%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        lenofs=len(s)
+        result=''
+        diff=base=2*numRows-2
+        if numRows==1:
+            return s
+        for i in range(numRows):
+            j=i
+            k=1
+            while j<lenofs:
+                result+=s[j]
+                if (k%2==0 and (base-diff)!=0) or diff==0:
+                    j+=(base-diff)
+                else:
+                    j+=diff
+                k+=1
+            diff-=2
+        return result
+
+
+
+
+
+
+
+        
