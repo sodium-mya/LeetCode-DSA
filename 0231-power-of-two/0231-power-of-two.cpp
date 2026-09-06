052155ad9d0620fae2eacca286c2f743d9c3cbc4
Namya Baranwal <deepnevy@gmail.com>
2026-09-06 15:23:09 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.84%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n==1 || n==2) return true;
+        else if (n==0) return false;
+        else if (n%2 ==0 && isPowerOfTwo(n/2)) return true;
+        else return false;
+    }
+};
