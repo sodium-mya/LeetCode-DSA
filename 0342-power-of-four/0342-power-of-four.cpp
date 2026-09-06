5e37d95855b47f584f9fb94e7e75d0c047a57346
Namya Baranwal <deepnevy@gmail.com>
2026-09-06 15:26:37 +0530

    Time: 0 ms (100%), Space: 7.8 MB (80.36%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n==1 || n==4) return true;
+        else if (n==0) return false;
+        else if (n%4 ==0 && isPowerOfFour(n/4)) return true;
+        else return false;
+        
+    }
+};
