d15a9d69a6d4ad5e9873d94b721b52756556b942
Namya Baranwal <deepnevy@gmail.com>
2026-09-06 15:25:54 +0530

    Time: 3 ms (59.85%), Space: 9 MB (9.85%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n==1 || n==3) return true;
+        else if (n==0) return false;
+        else if (n%3 ==0 && isPowerOfThree(n/3)) return true;
+        else return false;
+    }
+};
