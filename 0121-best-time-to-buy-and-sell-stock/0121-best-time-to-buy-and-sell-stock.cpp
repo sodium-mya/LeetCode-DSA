cd8e56d46d03fcedaa8efb326d0cf71d598b0431
Namya Baranwal <deepnevy@gmail.com>
2026-09-21 13:50:39 +0530

    Time: 0 ms (100%), Space: 97.4 MB (61.8%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_price=10000;
+        int max_profit=0;
+        for (int price : prices){
+            if (price < min_price) min_price = price;
+            else if ((price-min_price) > max_profit) {
+                max_profit = price - min_price;
+            }
+        }
+        return max_profit;
+    }
+};
